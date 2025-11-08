f3f2f7772b29e1339f2fa00c43d9488b696aee38
Bartosz Sławecki <bartosz@ilikepython.com>
2025-11-08 15:08:25 +0100

    Update stuff.py

--- stuff.py
+++ stuff.py
@@ -1,19 +1,17 @@
+# /// script
+# dependencies = [
+#     "typeshed_client>=2.8.2",
+# ]
+# ///
+
 import pprint
 import sys
-from pathlib import Path
 
 import typeshed_client
 
-context = typeshed_client.SearchContext(
-    typeshed=(typeshed := Path("./typeshed/stdlib")),
-    search_path=[],
-    version=sys.version_info[:2],
-    platform="linux"
-)
-
 stdlib_modules = set(sys.stdlib_module_names)
 
-for full_name, path in typeshed_client.get_all_stub_files(search_context=context):
+for full_name, path in typeshed_client.get_all_stub_files():
     name, nested, rest = full_name.partition(".")
     if nested:
         continue

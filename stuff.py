2c4ad7ddefd90f45a27e6d332ea4b33b424e0f85
Bartosz Sławecki <bartosz@ilikepython.com>
2025-11-08 12:20:33 +0100

    Try tracebacks
    Replace multiline string handling with file operations.

--- stuff.py
+++ stuff.py
@@ -1,6 +1,9 @@
-multiline = """
-abcde
+import linecache, pathlib, runpy
 
-"""
-
-print(repr(multiline))  # does it use os.linesep?
+p = pathlib.Path("t.py")
+p.write_text("1 / 0")
+try:
+    linecache.updatecache(str(p))
+    runpy.run_path(str(p))
+finally:
+    p.unlink()
